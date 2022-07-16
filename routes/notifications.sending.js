47f08f0cf1a1589d1025d5154ba2c03ee369b635
Andres Carrasquero <acarrasquero@modu.one>
2022-07-15 20:38:56 -0400

    updated for 0.9.0, ringing button

--- routes/notifications.sending.js
+++ routes/notifications.sending.js
@@ -1,4 +1,6 @@
+const { default: axios } = require("axios");
 const express = require("express");
+const { userDataRef } = require("../services/Firebase/Firebase.firestore");
 const { sendNewNotificationTo, sendNewNotificationToFCM, validateNotCode } = require("../utils/utilsFunctions");
 
 const router = express.Router();
@@ -15,24 +17,74 @@ router.post("/send", async (req, res) => {
     receiver,
     orderPost
   )
-  .then(async ({id}) => {
-    console.log("notification-sended-to -> ", receiver)
-    if(metadata?.notCode){
-      await validateNotCode(metadata?.notCode, {notid: id, email: receiver}).then((res) => {
-        res.json(res)
+    .then(async ({ id }) => {
+      console.log("notification-sended-to -> ", receiver)
+      if (metadata?.notCode) {
+        await validateNotCode(metadata?.notCode, { notid: id, email: receiver }).then((res) => {
+          res.json(res)
+        })
+      }
+      res.json({
+        status: 'notification-sended',
+        error: null
       })
-    }
-    res.json({
-      status: 'notification-sended',
-      error: null
     })
-  })
-  .catch((error) => {
-    res.json({
+    .catch((error) => {
+      res.json({
         status: 'error',
         error
+      })
     })
-  })
+});
+
+
+router.post("/order-ring-sound", async (req, res) => {
+  const { orderId, metadata, receiver } = req.body;
+  await sendNewNotificationTo(
+    "El experto ha llegado!",
+    "Recibe a el experto que haz solicitado, pronto tu averia sera solucionada!",
+    "system",
+    "orders",
+    "user",
+    null,
+    receiver,
+    null
+  )
+    .then(async ({ id }) => {
+      await userDataRef
+        .doc(receiver)
+        .collection("Orders")
+        .doc(`${orderId}`)
+        .update({ rignSound: "ring" })
+        .then(async () => {
+          setTimeout(async () => {
+            await userDataRef
+              .doc(receiver)
+              .collection("Orders")
+              .doc(`${orderId}`)
+              .update({ rignSound: null })
+              .then(async () => {
+                console.log("ringing-to -> ", receiver)
+                if (metadata?.notCode) {
+                  await validateNotCode(metadata?.notCode, { notid: id, email: receiver }).then((res) => {
+                    res.json(res)
+                  })
+                }
+                res.json({
+                  status: 'ringing-to-' + receiver,
+                  error: null
+                })
+              })
+          }, 3000);
+        })
+
+    })
+    .catch((error) => {
+      res.json({
+        status: 'error',
+        error
+      })
+    })
 });
 
 module.exports = router;
--- utils/constants.js
+++ utils/constants.js
@@ -1,31 +1,31 @@
 const { userDataRef } = require("../services/Firebase/Firebase.firestore")
 
 const NOTIFICATIONS_CODES = [
-    {
-        code: 'disable-0001',
-        function: (params) => new Promise((response, reject) => {
-            setTimeout(() => {
-                userDataRef
-                .doc(params.email)
-                .collection('UserNotifications')
-                .doc(params.notid)
-                .update({ viewed: true })
-                .then((doc) => {
-                    response({
-                        status: "notification disabled with delay 20000",
-                        error: null
-                    })
-                }).catch((error) => {
-                    reject({
-                        status: 'error: '+error.message,
-                        error: error 
-                    })
-                })
-            }, 20000);
-        })
-    }
+	{
+		code: 'disable-0001',
+		function: (params) => new Promise((response, reject) => {
+			setTimeout(() => {
+				userDataRef
+					.doc(params.email)
+					.collection('UserNotifications')
+					.doc(params.notid)
+					.update({ viewed: true })
+					.then((doc) => {
+						response({
+							status: "notification disabled with delay 20000",
+							error: null
+						})
+					}).catch((error) => {
+						reject({
+							status: 'error: ' + error.message,
+							error: error
+						})
+					})
+			}, 20000);
+		})
+	}
 ]
 
 module.exports = {
-    NOTIFICATIONS_CODES
+	NOTIFICATIONS_CODES
 }
